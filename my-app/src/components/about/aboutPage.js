7d1918f65752fc4ec177d9dd2b5baef37ff46373
a.tishchenko <a.tishchenko@outlook.com>
2018-11-18 22:24:26 +0200

    router - first version

---
+++ my-app/src/components/about/aboutPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          This app uses the following technologies:
+          <ul>
+            <li>React</li>
+            <li>React Router</li>
+          </ul>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import About from "./components/about/aboutPage";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class RootComponent extends Component {
+  render() {
+    let Child;
+
+    switch (this.props.route) {
+      case "about":
+        Child = About;
+        break;
+      default:
+        Child = App;
+    }
+    return (
+      <div>
+        <Child />
+      </div>
+    );
+  }
+}
+
+function render() {
+  let route = window.location.hash.substr(1);
+  ReactDOM.render(
+    <RootComponent route={route} />,
+    document.getElementById("root")
+  );
+}
+
+window.addEventListener("hashchange", render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
